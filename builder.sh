3f4adae51e08e1ec6a57a0470998c435ab0324d6
iusmac <iusico.maxim@libero.it>
2022-03-22 13:40:06 +0100

    Upgrade to crDroid:12.1
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- builder.sh
+++ builder.sh
@@ -3,7 +3,7 @@
 bash ContainerizedAndroidBuilder/run.sh \
     --email 'iusico.maxim@libero.it' \
     --repo-url 'https://github.com/crdroidandroid/android.git' \
-    --repo-revision '12.0' \
+    --repo-revision '12.1' \
     --lunch-system 'lineage' \
     --lunch-device 'rova' \
     --lunch-flavor 'userdebug' \
